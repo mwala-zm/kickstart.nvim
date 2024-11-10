05204dda93c83fa045835af481ee6d18ecc0531f
Mwala Z Mulenga <mwalamulenga.zt@gmail.com>
2024-11-10 08:14:03 +0200

    add oil nvim

--- init.lua
+++ init.lua
@@ -238,7 +238,7 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',    opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -273,7 +273,7 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
@@ -319,7 +319,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -762,7 +762,7 @@ require('lazy').setup({
     enable = {
       terminal = true,
       legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-      migrations = true,        -- Handle deprecated options automatically
+      migrations = true, -- Handle deprecated options automatically
     },
     styles = {
       bold = true,
@@ -900,31 +900,33 @@ require('lazy').setup({
 --
 -- https://github.com/weizheheng/ror.nvim
 --
-require("ror").setup({
+require('ror').setup {
   test = {
     message = {
       -- These are the default title for nvim-notify
-      file = "Running test file...",
-      line = "Running single test..."
+      file = 'Running test file...',
+      line = 'Running single test...',
     },
     coverage = {
       -- To customize replace with the hex color you want for the highlight
       -- guibg=#354a39
-      up = "DiffAdd",
+      up = 'DiffAdd',
       -- guibg=#4a3536
-      down = "DiffDelete",
+      down = 'DiffDelete',
     },
     notification = {
       -- Using timeout false will replace the progress notification window
       -- Otherwise, the progress and the result will be a different notification window
-      timeout = false
+      timeout = false,
     },
-    pass_icon = "✅",
-    fail_icon = "❌"
-  }
-})
+    pass_icon = '✅',
+    fail_icon = '❌',
+  },
+}
 
-vim.keymap.set("n", "<Leader>rc", ":lua require('ror.commands').list_commands()<CR>", { silent = true })
+vim.keymap.set('n', '<Leader>rc', ":lua require('ror.commands').list_commands()<CR>", { silent = true })
+
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- NOTE: My custom keybindings
 --
